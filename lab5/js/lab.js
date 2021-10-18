d716597be44f673c5edec03bd28c7e5e2b1dbf51
Monique Salinas <mnsalina@ucsc.edu>
2021-10-18 16:15:30 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author : Monique Salinas <mnsalina@ucsc.edu>
+* Created : 18 October
+* License : Public Domain
+*/
+
+// Define Varibles
+make = "Buick";
+model = "Verano";
+color = "white";
+year = 2012;
+
+// calculate
+age = 2021 - 2012;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
