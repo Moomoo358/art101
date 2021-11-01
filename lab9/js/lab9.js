e034e7835b5fd9c45954acaff984eda1e3a2ef0b
Monique Salinas <mnsalina@ucsc.edu>
2021-11-01 15:45:54 -0700

    lab 9

---
+++ lab9/js/lab9.js
@@ -0,0 +1,20 @@
+/*
+*Author : Monique Salinas <mnsalina@ucsc.edu>
+*Created : 1 November
+*License : Public Domain
+*/
+
+document.getElementById("output");
+var outputEl = "output";
+
+document.createElement("button");
+
+
+var btn = document.createElement("button");
+btn.innerHTML = "click me";
+document.body.appendChild(btn);
+
+
+var para = document.createElement("P");
+para.innerText = "This is my new paragraph";
+document.body.appendChild(para);
